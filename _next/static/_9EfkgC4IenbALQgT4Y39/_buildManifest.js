5390cef69532af769d724473726ead5d9aefa5d8
MKorostoff <mkorostoff@gmail.com>
2022-02-20 23:50:14 +0000

    Deploying to github-pages from @ MKorostoff/testeract@4405a4c8433805e239caddddc431d484ef14674e 🚀

---
+++ _next/static/_9EfkgC4IenbALQgT4Y39/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/895-1e0c6751afdf718e.js","static/chunks/pages/index-a6b48d2851d620b3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
