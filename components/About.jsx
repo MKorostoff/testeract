4405a4c8433805e239caddddc431d484ef14674e
Matt Korostoff <MKorostoff@gmail.com>
2022-02-20 18:49:29 -0500

    Fix static image loading, add favicon, add alt text

--- components/About.jsx
+++ components/About.jsx
@@ -13,30 +13,30 @@ export default function About() {
       <p>For instance, say you were building an online store, and you wanted to generate a list of every product you plan to sell. Suppose you want to sell shirts in two sizes (large and small) and two colors (blue and green). The set of sets contains 4 items: large blue, small blue, large green, small green. This is called &quot;the <a href="https://www.sciencedirect.com/topics/computer-science/cartesian-product" target="_blank"> cartesian product</a>.&quot; For data with only two dimensions (like size and color) it&apos;s pretty easy to represent in a spreadsheet with columns and rows:</p> 
       
       <div className="image-wrapper">
-        <Image layout="responsive" src={TwoDimension}/>
+        <Image alt="Image of an table having three rows and three columns" layout="responsive" src={TwoDimension}/>
       </div>
 
       <p>But once you add more dimensions, it gets really hard to represent in a maintainable way. Suppose we also wanted to offer each color/size combo in different styles: vneck, crew neck, and turtle neck. You might figure the answer is to create multiple two dimensional tables:</p>
 
       <div className="image-wrapper">
-        <Image layout="responsive" src={ThreeDimension}/>
+        <Image alt="Image of three tables, each having three rows and three columns" layout="responsive" src={ThreeDimension}/>
       </div>
 
       <p>But what happens when we add a, fourth dimension?</p>
 
       <div className="image-wrapper">
-        <Image layout="responsive" src={FourDimension}/>
+        <Image alt="Image of six tables, each having three rows and three columns" layout="responsive" src={FourDimension}/>
       </div>
       
       <p>And a fifth dimension?</p>
 
       <div className="image-wrapper">
-        <Image layout="responsive" src={FiveDimension}/>
+        <Image alt="Image of twelve tables, each having three rows and three columns" layout="responsive" src={FiveDimension}/>
       </div>
       <p>NOW, after making all these tables, what if someone told you you had to add a new size? Really pause and think for a moment what that would take.</p>
       
       <div className="image-wrapper mj">
-        <Image layout="responsive" src={MJ}/>
+        <Image alt="Image of michael jordan crying in the rain" layout="responsive" src={MJ}/>
       </div>
 
       <p>This is what I mean when I say tables are &quot;not maintainable.&quot; You can brute force it once, but modifying it basically means starting over.</p>
